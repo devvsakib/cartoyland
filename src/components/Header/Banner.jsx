d592e4703b0ed3a3db7f0e91dbfb327e0145d170
DevvSakib <devvsakib@gmail.com>
2023-05-22 00:02:00 +0600

    btn fixed

--- src/components/Header/Banner.jsx
+++ src/components/Header/Banner.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { toast } from 'react-hot-toast'
 import { Link } from 'react-router-dom'
-import PrimaryButton from '../../../../frontend/src/components/Common/PrimaryButton'
+import PrimaryButton from '../Common/PrimaryButton'
 
 const Banner = () => {
   const [coupon, setCoupon] = useState('CT50')
