f91d7c7c3b00415fb7bd5b49c5921bc9b32723d8
DevvSakib <devvsakib@gmail.com>
2023-05-21 23:48:47 +0600

    clear all

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,26 @@
 import { useContext } from 'react'
 import { CartContext } from '../contexts/CartProvider'
 import { FaTrash } from 'react-icons/fa'
+import Swal from 'sweetalert2'
 
 const Cart = () => {
-    const { cartItems, totalPrice, removeFromCart } = useContext(CartContext)
+    const { cartItems, totalPrice, clearCart, removeFromCart } = useContext(CartContext)
+
+    const removeAllItems = () => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You want to clear your cart!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#ef233c',
+            cancelButtonColor: '#60A5FA',
+            confirmButtonText: 'Yes, clear it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                clearCart()
+            }
+        })
+    }
     return (
         <div className='pb-20'>
             <div className='max-w-[1280px] mx-auto px-4 sm:px-6 lg:px-8'>
@@ -18,7 +35,7 @@ const Cart = () => {
                                         <h2 className="text-xl font-semibold mt-2 flex-1">{item.name}</h2>
                                         <div className='flex gap-2 text-accent mt-2 items-center'>
                                             <p className="font-semibold text-lg">${item.price} <span className='text-primary text-[13px]'>x{item.quantity}</span></p>
-                                            <button className='p-2' onClick={()=> removeFromCart(item._id)}>
+                                            <button className='p-2' onClick={() => removeFromCart(item._id)}>
                                                 <FaTrash />
                                             </button>
                                         </div>
@@ -31,7 +48,6 @@ const Cart = () => {
                                 </div>
                         }
                     </div>
-
                     <div>
                         <div className="flex gap-5 items-center shadow-md rounded-md p-5 mt-5">
                             <div className="flex-1">
@@ -43,6 +59,12 @@ const Cart = () => {
 
                                 <h2 className="text-xl font-semibold mt-2">Total</h2>
                                 <p className="text-accent font-semibold mt-2">${totalPrice}</p>
+                                <button className="btns mt-5 w-full" onClick={removeAllItems}>
+                                    Clear Cart
+                                </button>
+                                <button className="btns mt-2 w-full">
+                                    Checkout
+                                </button>
                             </div>
                         </div>
                     </div>
